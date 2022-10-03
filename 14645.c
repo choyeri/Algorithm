f80809f33169e89c68fd33dde327925602e6f325
choyeri <dpfl010462@gmail.com>
2022-10-03 17:48:31 +0900

    BaekJoon 14645

---
+++ 14645.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, a, b;
+    scanf("%d %d", &n, &k);
+    for (i = 0; i < n; i++) {
+        scanf("%d %d", &a, &b);
+    }
+    printf("ºñ¿ÍÀÌ");
+    return 0;
+}
