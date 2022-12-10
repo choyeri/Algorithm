de3c45370db0883f8bfb283f0377e01b4e6c1ba6
choyeri <dpfl010462@gmail.com>
2022-12-10 23:49:37 +0900

    BaekJoon 10768 특별한 날

---
+++ 10768.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/*
+10768 특별한 날
+*/
+
+int main()
+{
+	int month, day;
+	cin >> month >> day;
+	if (month == 2) {
+		if (day == 18)
+			cout << "Special";
+		else if (day < 18)
+			cout << "Before";
+		else
+			cout << "After";
+	}
+	else if (month < 2)
+		cout << "Before";
+	else
+		cout << "After";
+}
